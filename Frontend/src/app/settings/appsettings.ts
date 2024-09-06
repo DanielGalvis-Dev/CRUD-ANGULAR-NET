441227bbf139a02baf621c8886cb874094a6ed91
Daniel Galvis <djgz2004@gmail.com>
2024-09-06 11:18:36 -0500

    add comments in the file appsettings.ts

--- Frontend/src/app/settings/appsettings.ts
+++ Frontend/src/app/settings/appsettings.ts
@@ -1,3 +1,6 @@
+// Exporta un objeto de configuración que puede ser importado en otras partes de la aplicación
 export const appsettings = {
+    // Define la URL base para la API de la aplicación
     apiUrl: "http://localhost:5016/api/"
-}
+  };
+  
