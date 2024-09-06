a7d4eda8c0d513d5019608f5682f077655f04406
Daniel Galvis <djgz2004@gmail.com>
2024-09-06 10:56:59 -0500

    add comment in the file app.config.ts

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,14 +1,20 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core'; 
+import { provideRouter, withComponentInputBinding } from '@angular/router'; 
 
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HttpClientModule } from '@angular/common/http';
+import { routes } from './app.routes'; // Importa las rutas definidas en el archivo app.routes.ts
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; 
+import { HttpClientModule } from '@angular/common/http'; // Módulo HTTP para manejar peticiones HTTP
 
+// Configuración principal de la aplicación Angular
 export const appConfig: ApplicationConfig = {
   providers: [
+    // Proveedor de enrutamiento: establece las rutas de la aplicación y permite el enlace de datos entre componentes
     provideRouter(routes, withComponentInputBinding()),
+
+    // Proveedor de animaciones asíncronas: optimiza el manejo de animaciones en la aplicación
     provideAnimationsAsync(),
+
+    // Proveedor de módulos externos: importa el módulo HttpClient para manejar solicitudes HTTP en toda la aplicación
     importProvidersFrom(HttpClientModule),
   ],
 };
