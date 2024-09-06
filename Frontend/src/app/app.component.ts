025debb7e576830246e2937e86cb8e0113cc5844
Daniel Galvis <djgz2004@gmail.com>
2024-09-06 11:05:27 -0500

    add comment in the file app.component.ts

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'; 
+import { RouterOutlet } from '@angular/router'; // RouterOutlet permite que las rutas dinámicas se carguen en este componente
 
+// Decorador @Component para definir el componente raíz de la aplicación
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root', // Nombre del componente que será utilizado en el HTML como <app-root></app-root>
+  standalone: true, // Configura el componente como standalone, lo que significa que no depende de un módulo
+  imports: [RouterOutlet], // Importa RouterOutlet para gestionar las rutas en este componente
+  templateUrl: './app.component.html', // Define la ruta del archivo de plantilla HTML asociado a este componente
+  styleUrls: ['./app.component.css'] // Define la ruta del archivo CSS que contiene los estilos específicos de este componente
 })
 export class AppComponent {
-  title = 'MiApp';
+  // Variable de título, se utiliza en la plantilla HTML para mostrar el título de la aplicación
+  title = 'MiApp'; 
 }
