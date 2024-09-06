68ed56d5e2458ffb3f9f0786f8b08f4577bbf7b2
Daniel Galvis <djgz2004@gmail.com>
2024-09-06 11:22:21 -0500

    add comments in the file Empleado.ts

--- Frontend/src/app/Models/Empleado.ts
+++ Frontend/src/app/Models/Empleado.ts
@@ -1,8 +1,9 @@
+// Define una interfaz para representar un empleado
 export interface Empleado {
-  idP: number; // Id del empleado
-  nombreP: string;
-  apellidoP: string;
-  correoP: string;
-  sueldoP: number;
-  fechaContratoP: Date;
+  idP: number; // Identificador único del empleado
+  nombreP: string; // Nombre del empleado
+  apellidoP: string; // Apellido del empleado
+  correoP: string; // Correo electrónico del empleado
+  sueldoP: number; // Sueldo del empleado
+  fechaContratoP: Date; // Fecha del contrato del empleado
 }
