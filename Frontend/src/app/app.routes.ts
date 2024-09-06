9f1ca32a1a603e7d7f5e404ff25d253af92ff449
Daniel Galvis <djgz2004@gmail.com>
2024-09-06 10:54:18 -0500

    add comment in the file app.routes.ts

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,15 @@
-import { Routes } from '@angular/router';
-import { InicioComponent } from './Views/inicio/inicio.component';
-import { EmpleadoComponent } from './Views/empleado/empleado.component';
+import { Routes } from '@angular/router'; 
+import { InicioComponent } from './Views/inicio/inicio.component'; 
+import { EmpleadoComponent } from './Views/empleado/empleado.component'; 
 
+// Definición de las rutas de la aplicación
 export const routes: Routes = [
+  // Ruta raíz: Cuando no se especifica ningún camino, se carga el componente InicioComponent
   { path: '', component: InicioComponent },
+  
+  // Ruta 'inicio': Redirige al componente InicioComponent
   { path: 'inicio', component: InicioComponent },
+  
+  // Ruta 'empleado/:id': Redirige al componente EmpleadoComponent, pasando un parámetro dinámico 'id'
   { path: 'empleado/:id', component: EmpleadoComponent },
 ];
