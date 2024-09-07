c16484358469cc10884009c79410bcc784404d93
Daniel Galvis <djgz2004@gmail.com>
2024-09-07 09:42:37 -0500

    Update DB

--- dbcrudangularnet.sql
+++ dbcrudangularnet.sql
@@ -1,31 +1,18 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 07-09-2024 a las 15:40:42
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Eliminar la base de datos si ya existe y crear una nueva
+DROP DATABASE IF EXISTS dbcrudangularnet;
+CREATE DATABASE dbcrudangularnet CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE dbcrudangularnet;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `dbcrudangularnet`
---
-
-DELIMITER $$
---
--- Procedimientos
---
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_createEmpleado` (IN `p_Nombre` VARCHAR(50), IN `p_Apellido` VARCHAR(50), IN `p_Correo` VARCHAR(100), IN `p_Sueldo` INT, IN `p_FechaContrato` VARCHAR(10))   BEGIN
+-- Procedimiento almacenado para insertar un nuevo empleado
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_createEmpleado` (
+    IN `p_Nombre` VARCHAR(50),
+    IN `p_Apellido` VARCHAR(50),
+    IN `p_Correo` VARCHAR(100),
+    IN `p_Sueldo` INT,
+    IN `p_FechaContrato` VARCHAR(10)
+)
+BEGIN
+    -- Insertar empleado en la tabla con la fecha convertida al formato de fecha de MySQL
     INSERT INTO empleados (
         nombre,
         apellido,
@@ -40,23 +27,42 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_createEmpleado` (IN `p_Nombre` V
         p_Sueldo,
         STR_TO_DATE(p_FechaContrato, '%d/%m/%Y')
     );
-END$$
+END;
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_deleteEmpleado` (IN `p_Id` INT)   BEGIN
+-- Procedimiento almacenado para eliminar un empleado por su ID
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_deleteEmpleado` (IN `p_Id` INT)
+BEGIN
+    -- Eliminar empleado de la tabla por su ID
     DELETE FROM empleados
     WHERE id = p_Id;
-END$$
+END;
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_getEmpleado` (IN `p_Id` INT)   BEGIN
+-- Procedimiento almacenado para obtener los datos de un empleado por su ID
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_getEmpleado` (IN `p_Id` INT)
+BEGIN
+    -- Seleccionar un empleado de la tabla usando el ID proporcionado
     SELECT * FROM empleados
     WHERE id = p_Id;
-END$$
+END;
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_readEmpleados` ()   BEGIN
+-- Procedimiento almacenado para obtener la lista completa de empleados
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_readEmpleados` ()
+BEGIN
+    -- Seleccionar todos los empleados de la tabla
     SELECT * FROM empleados;
-END$$
+END;
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_updateEmpleado` (IN `p_Id` INT, IN `p_Nombre` VARCHAR(50), IN `p_Apellido` VARCHAR(50), IN `p_Correo` VARCHAR(100), IN `p_Sueldo` INT, IN `p_FechaContrato` VARCHAR(10))   BEGIN
+-- Procedimiento almacenado para actualizar los datos de un empleado
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_updateEmpleado` (
+    IN `p_Id` INT,
+    IN `p_Nombre` VARCHAR(50),
+    IN `p_Apellido` VARCHAR(50),
+    IN `p_Correo` VARCHAR(100),
+    IN `p_Sueldo` INT,
+    IN `p_FechaContrato` VARCHAR(10)
+)
+BEGIN
+    -- Actualizar los datos del empleado con la información proporcionada
     UPDATE empleados SET
         nombre = p_Nombre,
         apellido = p_Apellido,
@@ -64,46 +70,14 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_updateEmpleado` (IN `p_Id` INT, 
         sueldo = p_Sueldo,
         fechaContrato = STR_TO_DATE(p_FechaContrato, '%d/%m/%Y')
     WHERE id = p_Id;
-END$$
+END;
 
-DELIMITER ;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `empleados`
---
-
+-- Crear la tabla empleados con las columnas necesarias
 CREATE TABLE `empleados` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `apellido` varchar(50) NOT NULL,
-  `correo` varchar(100) NOT NULL,
-  `sueldo` int(10) NOT NULL,
-  `fechaContrato` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `empleados`
---
-ALTER TABLE `empleados`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `empleados`
---
-ALTER TABLE `empleados`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT, -- ID único para cada empleado
+  `nombre` varchar(50) NOT NULL, -- Nombre del empleado
+  `apellido` varchar(50) NOT NULL, -- Apellido del empleado
+  `correo` varchar(100) NOT NULL, -- Correo electrónico del empleado
+  `sueldo` int(10) NOT NULL, -- Sueldo del empleado
+  `fechaContrato` date DEFAULT NULL -- Fecha de contrato del empleado
+)
